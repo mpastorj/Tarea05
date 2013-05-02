c9ea51c2d4be9f557207145e269a5b4b50b56898
mpastorj <mrlyn.p.j@gmail.com>
2013-05-01 22:04:44 -0400

    Clase conexion

---
+++ Conexion.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <libpq-fe.h>
+
+using namespace std;
+
+PGconn *cnn = NULL;
+PGresult *result = NULL;
+
+class Conexion{
+
+ int i;
+
+ public:
+
+ void conectar();
+ void consulta(const char *consulta);    
+ void desconectar();   
+
+};
+
+
+void Conexion::conectar(){
+
+cnn = PQsetdbLogin("sebastian.cl","5432",NULL,NULL,"iswdb","isw","isw"); // Se crea la conexión con los datos de nuestro servidor
+if (PQstatus(cnn) != CONNECTION_BAD) {
+        cout << "Estamos conectados a PostgreSQL!" << endl;
+        }
+else {
+        cout << "Error de conexion" << endl;
+    }
+
+}
+
+
+void Conexion::consulta(const char *consulta){
+
+result = PQexec(cnn, consulta);
+
+if (result != NULL) {
+            int tuplas = PQntuples(result);
+            int campos = PQnfields(result);
+            cout << "No. Filas:" << tuplas << endl;
+            cout << "No. Columnas:" << campos << endl;
+
+            cout << "Los nombres de las columnas son:" << endl;
+ 	    cout << "--------------------------------" << endl;
+
+            for (i=0; i<campos; i++) {
+                cout << PQfname(result,i) << " | ";
+            }
+
+            cout << endl << "--------------------------------" << endl;
+
+            for (i=0; i<tuplas; i++) {
+                for (int j=0; j<campos; j++) {
+                    cout << PQgetvalue(result,i,j) << " | ";
+                }
+                cout << endl;
+            }
+        }
+
+}
+
+void Conexion::desconectar(){
+
+   PQclear(result);
+   PQfinish(cnn);
+
+cout<<endl<<"Conexión finalizada..."<<endl;
+
+}
